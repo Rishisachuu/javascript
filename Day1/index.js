7f2e6a61176ccb2bfa4233d03e18e67709b59725
Rishika <rishigamanigandan1@gmail.com>
2025-07-12 10:42:58 +0530

    Create index.js

---
+++ Day1/index.js
@@ -0,0 +1,34 @@
+const number=12;
+const name=`js`;
+const iscloudy=true;
+
+console.log(number+" "+name+" "+iscloudy);
+document.write(number+" "+name+" "+iscloudy);
+
+//task 2
+console.log(typeof(number));
+console.log(typeof(name));
+console.log(typeof(iscloudy));
+
+//task 3
+//addition
+//const a=12;
+//const b=31;
+const a=prompt(("enter the first value"));
+const b=prompt(("enter the second number"));
+alert(a+b);
+console.log(a+b);
+document.write(a+b);
+
+//subtraction
+console.log(a-b);
+document.write(a-b);
+
+//multiplication
+console.log(a*b);
+document.write(a*b);
+
+//division
+console.log(a/b);
+document.write(a/b);
+
