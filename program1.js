b31d20c4c80d0aa2345a937ae99735f36deaa38c
Rishika <rishigamanigandan1@gmail.com>
2025-07-29 09:16:44 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -27,7 +27,7 @@
   <script>
     function calculate(){
         let num1=parseFloat(document.getElementById("num1").value);
-        let num2=parseFloat(document.getElementById("num1").value);
+        let num2=parseFloat(document.getElementById("num2").value);
         let operation=document.getElementById("operation").value;
         let result;
         if(isNaN(num1)||isNaN(num2)){
