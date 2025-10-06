af71ecf4912807e5f13c331c8e1aada788527ff7
Rishika <rishigamanigandan1@gmail.com>
2025-10-06 10:26:00 +0530

    Add files via upload

---
+++ Program/script.js
@@ -0,0 +1,7 @@
+let images=["https://r1.ilikewallpaper.net/iphone-wallpapers/download/87833/cristiano-ronaldo-iphone-iphone-wallpaper-ilikewallpaper_com.jpg","https://r1.ilikewallpaper.net/iphone-wallpapers/download/87836/51-Cristiano-Ronaldo-2020-Mobile-on-afari-iphone-wallpaper-ilikewallpaper_com.jpg","https://wallpapers.com/images/high/kneeling-cristiano-ronaldo-iphone-48yijdyl8kvhsx4y.web.jpg"]
+document.querySelectorAll(".but button")[0].addEventListener('click',()=>{
+    document.querySelector(".carousel img").scr="https://r1.ilikewallpaper.net/iphone-wallpapers/download/87836/51-Cristiano-Ronaldo-2020-Mobile-on-afari-iphone-wallpaper-ilikewallpaper_com.jpg"
+})
+document.querySelectorAll(".but button")[1].addEventListener('click',()=>{
+    document.querySelector(".carousel img").scr="https://wallpapers.com/images/high/kneeling-cristiano-ronaldo-iphone-48yijdyl8kvhsx4y.web.jpg"
+})
